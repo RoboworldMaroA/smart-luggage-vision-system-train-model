8bb5a1954d303403f4e67f8481124d25841dd197
Maro <you@example.com>
2023-02-19 11:11:06 +0000

    add README and File to execute program

--- ObjectRecognitionVisionSystemInProject.sh
+++ ObjectRecognitionVisionSystemInProject.sh
@@ -4,4 +4,8 @@ cd ~
 cd Desktop
 cd visionSystemObjectRecognition
 source bin/activate
+
+#python3 object_detect_sift_3_objects.py
+#python3 popUpWindow.py
+python3 crop_image_from_camera.py
 python3 object_detect_sift_marek_photo_3_objects_mod.py
---
+++ README.md
@@ -0,0 +1,2 @@
+Application used to recognize object using camera, object is photographed and then will be recognize by camera.
+
